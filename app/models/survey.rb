246868f27163f067a1ef997fc9b36c184f708707
jonathanstar <jstar13@cmc.edu>
2015-10-30 15:33:20 -0400

    add active record associations

--- app/models/survey.rb
+++ app/models/survey.rb
@@ -2,6 +2,7 @@ class Survey < ActiveRecord::Base
   belongs_to :user, foreign_key: :creator_id
   has_many   :questions
   has_many   :responses
+  has_many   :choices, through: :responses
 
   validates :creator_id, presence: true
   validates :title, presence: true
