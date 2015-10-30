fbf736767a4a550756242097347c3f7d7b9e30b8
jonathanstar <jstar13@cmc.edu>
2015-10-30 16:13:35 -0400

    rspec test for username/password authentication

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'User' do
   it 'should have a password' do
-    steffi = User.create!({username: "steffi", password: "stef"})
+    steffi = User.create!({name: "steffi", password: "stef"})
     expect(steffi.password).to eq("stef")
     expect(steffi.password_digest).to_not eq('stef')
   end
