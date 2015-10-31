54bc41214c6cbe25d98376e7eb31e591dc421553
Rachel Lim <email": null>
2015-10-31 14:55:53 -0400

    trash

--- app/controllers/choice.rb
+++ app/controllers/choice.rb
@@ -1,6 +1,30 @@
+
 get '/choices/:id' do
   choice = Choice.where(id: params[:id]).first
   return "choice with id #{params[:id]} is not a choice object" unless choice.is_a?(Choice)
   choice.delete
   redirect '/survey/1'
 end
+
+get '/choice' do
+  choice = Choice.new(params[:choice])
+  erb :'questions/_new_choice', locals: {choice: choice}, layout: false
+end
+
+post '/choice' do
+  @question = Question.find(params[:choice][:question_id])
+  choice = Choice.new(params[:choice])
+  if choice.save
+    erb :'questions/_new_choice', locals: {choice: choice}, layout: false
+  else
+    @errors = choice.errors.full_messages
+    erb :'questions/_new_choice'
+  end
+end
+
+post '/choice/new' do
+  @question = Question.new(params[:question])
+    erb :'questions/_new_question', layout: false
+end
+
+
--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -1,4 +1,5 @@
 
+
 get '/survey/:survey_id/results' do
   survey = Survey.find(params[:survey_id])
   survey_questions = survey.questions
@@ -11,17 +12,18 @@ get '/survey/new' do
   erb :'/survey/new_survey'
 end
 
+
 post '/survey' do
   user = User.find(session[:user_id])
   survey = Survey.new(creator_id: user.id, title: params[:survey][:title])
 
-    if survey.save
-      erb :'/questions/_new_question', locals: {survey: survey}, layout: false
-    else
-      @errors = survey.errors.full_messages
-      erb :'/survey/new_survey'
-    end
+  if survey.save
+    erb :'/questions/_new_question', locals: {survey: survey}, layout: false
+  else
+    @errors = survey.errors.full_messages
+    erb :'/survey/new_survey', layout: false
   end
+end
 
 
 get '/survey/:survey_id' do
@@ -29,12 +31,9 @@ get '/survey/:survey_id' do
   survey_questions = survey.questions
 
   erb :'/survey/view', locals: {survey: survey, survey_questions: survey_questions}
+
 end
 
-get '/question' do
-  question = Question.new(params[:question])
-  erb :'/questions/_new_question', locals: {question: question}
-end
 
 
 
@@ -48,14 +47,6 @@ post '/question' do
   end
 end
 
-post '/choice' do
-  choice = Choice.new(params[:choice])
-  if choice.save
-    erb :'questions/_new_choice', locals: {choice: choice}
-  else
-    @errors = choice.errors.full_messages
-    erb :'questions/_new_choice'
-  end
-end
+
 
 
