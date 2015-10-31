d45b55a2ffd31328b2295cc646a5e3f8d775e7d5
Rachel Lim <email": null>
2015-10-31 15:00:41 -0400

    fix merge conflict

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -1,3 +1,4 @@
+
 get '/survey/:survey_id/results' do
   survey = Survey.find(params[:survey_id])
   survey_questions = survey.questions
@@ -6,12 +7,55 @@ get '/survey/:survey_id/results' do
   erb :'/survey/results', locals: {survey: survey, survey_questions: survey_questions}
 end
 
+get '/survey/new' do
+  erb :'/survey/new_survey'
+end
+
+post '/survey' do
+  user = User.find(session[:user_id])
+  survey = Survey.new(creator_id: user.id, title: params[:survey][:title])
+
+    if survey.save
+      erb :'/questions/_new_question', locals: {survey: survey}, layout: false
+    else
+      @errors = survey.errors.full_messages
+      erb :'/survey/new_survey'
+    end
+  end
+
+
 get '/survey/:survey_id' do
   survey = Survey.find(params[:survey_id])
   survey_questions = survey.questions
 
   erb :'/survey/view', locals: {survey: survey, survey_questions: survey_questions}
+end
 
+get '/question' do
+  question = Question.new(params[:question])
+  erb :'/questions/_new_question', locals: {question: question}
+end
+
+
+
+post '/question' do
+  question = Question.new(params[:question])
+
+  if question.save
+    erb :"/questions/_new_choice"
+  else
+    @errors = question.errors.full_messages
+  end
+end
+
+post '/choice' do
+  choice = Choice.new(params[:choice])
+  if choice.save
+    erb :'questions/_new_choice', locals: {choice: choice}
+  else
+    @errors = choice.errors.full_messages
+    erb :'questions/_new_choice'
+  end
 end
 
 
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,41 @@
 $(document).ready(function() {
 
+  $(".survey_form").on("submit", function(event) {
+    event.preventDefault();
+
+    var survey_form = $(".survey_form").serialize();
+
+    var request = $.ajax({
+      method: "post",
+      url: "/survey",
+      data: survey_form,
+    })
+
+    request.done(function(survey_form){
+      // $(".survey_form").toggle();
+      $(".questions").append(survey_form)
+    })
+
+  })
+
+
+
+  $(".add-choice").on("submit", function(event) {
+    event.preventDefault();
+
+    var question_form = $(".question_form").serialize();
+
+    var request = $.ajax({
+      method: "post",
+      url: "/question",
+      data: question_form,
+    })
+
+    request.done(function(question_data) {
+
+
+    })
+
+  })
+
 });
