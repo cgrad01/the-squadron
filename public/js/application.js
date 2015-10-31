8472366fe1f6d9e95bc9d0528c6212aa0e9279fe
jonathanstar <jstar13@cmc.edu>
2015-10-31 16:15:18 -0400

    ajax delete choice

--- public/js/application.js
+++ public/js/application.js
@@ -1,85 +1,26 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 
-// Add more questions to survey
-  // $(document).on("click", ".next_question", function(event){
-  //   event.preventDefault();
+  $(document).on("click", ".delete-choice", function(){
+    event.preventDefault();
+    $target=$(event.target)
+    var choiceId = $target.attr("href").slice(9)
 
-  //   // var question_form = $(".question_form").serialize();
+    var request = $.ajax({
+      method: "post",
+      url: "/choices/"+ choiceId,
+      data: $(this).serialize()
+    });
 
-  //   var request = $.ajax({
-  //     method: "post",
-  //     url: "/question",
-  //     // data: question_form,
-  //   })
+    request.done(function(){
+      $target.parent().parent().remove()
+    });
 
-  //   request.done(function(question_form) {
+    request.fail(function(jsXHR){
+      console.log(jsXHR.responseText)
+      console.log("fail")
+    });
 
-  //     $(".questions").append(question_form)
-  //   })
-
-  // })
-
-// Adds more choices to first question
-  // $(document).on("submit", ".choice_form", function(event) {
-  //   event.preventDefault();
-
-  //   var choice_form = $(".choice_form").serialize();
-
-  //   var request = $.ajax({
-  //     method: "post",
-  //     url: "/choice",
-  //     data: choice_form,
-  //   })
-
-  //   request.done(function(choice_data) {
-  //     $(".choices").append(choice_data)
-
-  //   })
-  // })
-
-// Adds first choice to first question
-  // $(document).on("submit", ".question_form", function(event) {
-
-  //   event.preventDefault();
-
-  //   var question_form = $(".question_form").serialize();
-
-  //   var request = $.ajax({
-  //     method: "post",
-  //     url: "/question",
-  //     data: question_form,
-  //   })
-
-  //   request.done(function(choice_data) {
-
-  //     $(".questions > .choices").append(choice_data)
-  //   })
-
-  // })
-
-// Adds first question to survey
-  // $(document).on("submit", ".survey_form", function(event) {
-  //   event.preventDefault();
-
-  //   var survey_form = $(".survey_form").serialize();
-  //   var choice_form = $('.choice_form').serialize();
-
-  //   var request = $.ajax({
-  //     method: "post",
-  //     url: "/survey",
-  //     data: survey_form,
-  //   })
-  //   request.done(function(question_form){
-  //     $(".questions").append(question_form)
-  //   })
-
-  //   request.fail(function(question_form) {
-  //     console.log("fail.");
-
-  //   })
-
-  // })
+  });
 
 
-
-// });
+});
