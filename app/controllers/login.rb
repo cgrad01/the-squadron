02ec182da10b4e25d9eecc35fe49d773a566a5ff
jonathanstar <jstar13@cmc.edu>
2015-11-01 00:10:03 -0400

    fix broken link to profiles/:id

--- app/controllers/login.rb
+++ app/controllers/login.rb
@@ -11,7 +11,7 @@ post '/users' do
     user = User.new(params[:user])
     if user.save
       session[:user_id] = user.id
-      redirect "/profile/#{user.id}"
+      redirect "/profiles/#{user.id}"
     else
       @errors = user.errors.full_messages
       erb :login, locals: {user: user}
